229898a6d2edff7f56bf91e965c39518c7ca3ac7
sungwon <jsw2095@naver.com>
2018-07-14 15:25:32 +0900

    init

--- Gemfile
+++ Gemfile
@@ -7,7 +7,9 @@ ruby '2.4.2'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'jquery-rails'
-gem 'sqlite3'
+gem 'sqlite3', :group => :development
+gem 'pg', '~> 0.21', :group => :production
+gem 'rails_12factor', :group => :production
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    collection do
+      get :first
+      get :second
+      get :third
+      get :firth
+      get :fifth
+      get :sixth
+      get :seventh
+      get :eighth
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
